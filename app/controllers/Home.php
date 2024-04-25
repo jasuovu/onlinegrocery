0942cfc49ae109ddd47453f7960b6ab98e8a093c
honeybuttercrisp <vietdragon.jv@gmail.com>
2024-04-25 16:14:26 +1000

    changes

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,8 +1,20 @@
 <?php
 
 class Home{
-    function __construct()
+    public function index()
     {
         echo 'Home page';
     }
+
+    public function edit()
+    {
+        echo 'Home editing';
+    }
+
+    public function delete()
+    {
+        echo 'Home delete';
+    }
+
+
 }
