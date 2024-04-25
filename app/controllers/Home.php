4f5c911688b9168cfda1b3ac8e0181366d8da357
honeybuttercrisp <vietdragon.jv@gmail.com>
2024-04-25 13:56:15 +1000

    first commit

---
+++ app/controllers/Home.php
@@ -0,0 +1,8 @@
+<?php
+
+class Home{
+    function __construct()
+    {
+        echo 'Home page';
+    }
+}
---
+++ app/controllers/_404.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * 404 class page not found
+ */
+
+class _404
+{
+    function __construct()
+    {
+        echo '404 page not found';
+    }
+}
